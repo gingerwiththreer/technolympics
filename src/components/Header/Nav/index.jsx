3e3c03436e1869be53332fc068c7c179632ebd42
Samrat Choudhary <samrat.choudhary757@gmail.com>
2024-09-14 21:19:58 +0530

    yes

--- src/components/Header/Nav/index.jsx
+++ src/components/Header/Nav/index.jsx
@@ -1,57 +1,57 @@
 import styles from './style.module.scss';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 import { links, footerLinks } from './data';
-import { perspective, slideIn } from "./anim";
+// import { perspective, slideIn } from "./anim";
 
 export default function index() {
   return (
     <div className={styles.nav}>
        <div className={styles.body}>
         {
-            links.map( (link, i) => {
+            links.map( (link) => {
                 const { title, href, target } = link;
                 return (
-                    <div key={`b_${i}`} className={styles.linkContainer}>
-                        <motion.a
+                    <div className={styles.linkContainer}>
+                        <a
                         
                           href={href}
                           target={target}
-                          custom={i}
-                          variants={perspective}
-                          initial="initial"
-                          animate="enter"
-                          exit="exit"
+                        //   custom={i}
+                        //   variants={perspective}
+                        //   initial="initial"
+                        //   animate="enter"
+                        //   exit="exit"
                         >
                             
                             {title}
                             
-                        </motion.a>
+                        </a>
                     </div>
                 )
             })
         }
        </div>
-       <motion.div className={styles.footer}>
+       <div className={styles.footer}>
             {
-                footerLinks.map( (link, i) => {
+                footerLinks.map( (link) => {
                     const { title, href, target } = link;
                     return (
-                        <motion.a 
+                        <a 
                             href={href}
                             target={target}
-                            variants={slideIn}
-                            custom={i} 
-                            initial="initial"
-                            animate="enter"
-                            exit="exit"
-                            key={`f_${i}`}
+                            // variants={slideIn}
+                            // custom={i} 
+                            // initial="initial"
+                            // animate="enter"
+                            // exit="exit"
+                            // key={`f_${i}`}
                         >
                             {title}
-                        </motion.a>
+                        </a>
                     )
                 })
             }
-       </motion.div>
+       </div>
     </div>
   )
 }
