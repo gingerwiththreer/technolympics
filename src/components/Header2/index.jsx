d56f163ffde594deaab7e12609dbc4067cb88180
Samrat Choudhary <89210535+gingerwiththreer@users.noreply.github.com>
2024-10-22 15:01:06 +0530

    Update index.jsx

--- src/components/Header2/index.jsx
+++ src/components/Header2/index.jsx
@@ -23,7 +23,7 @@ function Header2() {
                 <Countdown targetDate="2024-12-25T00:00:00" /> 
             </div>
             <h1 className={styles.date}>
-                22nd October 2024
+                COMING SOON
             </h1>
         </div>
         
