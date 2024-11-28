cb5433a4f747bd00fbb1aa4fc153b5008dadeab1
Samrat Choudhary <89210535+gingerwiththreer@users.noreply.github.com>
2024-11-28 23:00:51 +0530

    Update index.jsx

--- src/components/Header2/index.jsx
+++ src/components/Header2/index.jsx
@@ -20,7 +20,7 @@ function Header2() {
             <img className={styles.imgphone} src="https://pbs.twimg.com/media/GXgRJWsWoAAPtX0?format=png&name=medium" />
 
             <div className={styles.timer}>
-                <Countdown targetDate="2024-11-26T00:00:00" /> 
+                <Countdown targetDate="2024-11-29T00:00:00" /> 
             </div>
             <h1 className={styles.date}>
                 NOVEMBER 29
