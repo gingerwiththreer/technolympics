d5920788b849bac04fd08bed579f011fc67de014
Samrat Choudhary <89210535+gingerwiththreer@users.noreply.github.com>
2025-02-03 23:53:06 +0530

    Update index.jsx

--- src/components/Header2/index.jsx
+++ src/components/Header2/index.jsx
@@ -23,7 +23,7 @@ function Header2() {
                 <Countdown targetDate="2024-11-29T14:00:00" /> 
             </div>
             <h1 className={styles.date}>
-                END OF AN ERA
+                29 NOV
             </h1>
         </div>
         
